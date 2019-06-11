e61d7ac480a52d0b344446c0ef9b9ca38da49301
Jack Huffman <35504266+jack-huffman@users.noreply.github.com>
2019-06-11 12:44:34 -0500

    Add files via upload

---
+++ README.ml
@@ -0,0 +1,34 @@
+War Instructions
+Card game by Jackson Huffman
+
+The card game is played by taking a deck of 52 cards, and shuffling them.
+The deck is then dealt to two players until each player has 26 cards.
+the players take their deck, with all the cards upside down, and at the same time,
+flip the top card over. The players compare these two cards, and the player with
+the higher card takes them both. The cards, ranked, and from smallest to greatest are:
+
+2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A
+
+The name, war, comes from what happens when the two randomly flipped cards happen
+to be the same. When this happens, the players then each flip two more cards over,
+and then compare the newest card flipped. The winner of this comparison then takes
+the pile of cards in the middle. This process, called war, is repeated for every
+tie between the players.
+
+This process of comparing goes on until one player has all the cards.
+
+This game is notorious for taking forever to complete, as there is no skill involved.
+The game is entirely based on luck, but to make the game easier to win, I changed
+the response to players tying. When players tie, I flip two additional cards, plus
+three more, putting a total of 6 cards per player in the pile. This means that tying
+greatly changes the dynamic of the game, and makes the game actually winnable.
+
+
+How to Play
+
+Playing the game is extremely easy. One only needs to click the "Flip" button to
+compare cards between players. I recommend moving the main window so that the "Flip"
+button aligns with the outcome dialoge box per outcome, as this makes it much easier
+to flip cards fast. The textbox in the bottom right displays the player's score, and
+the textbox in the top right displays the opponent's score. The game ends when one
+player has 52 cards.
